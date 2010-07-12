a244dfb9739ee3f5810665cf4adbe648cd81f310
masanao (Masanao Yajima) <yajima@stat.ucla.edu>
2010-07-12 19:41:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/apl@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aplEncode.R
+++ pkg/R/aplEncode.R
@@ -2,6 +2,6 @@ aplEncode <-
 function(ind,dims) {
 n<-length(dims); cell<-integer(n)
 if ((ind < 1) || (ind > prod(dims))) stop("No such cell")
-.C("aplEncodeC ",as.integer(cell),as.integer(dims),as.integer(n),as.integer(ind))[[1]]
+.C("aplEncodeC",as.integer(cell),as.integer(dims),as.integer(n),as.integer(ind))[[1]]
 }
 
