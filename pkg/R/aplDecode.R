39e769a7d668989fd2f603e128e3bf1e4d7b8f55
masanao (Masanao Yajima) <yajima@stat.ucla.edu>
2010-07-14 19:29:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/apl@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aplDecode.R
+++ pkg/R/aplDecode.R
@@ -1,7 +1,16 @@
-aplDecode <-
-function(cell,dims) {
-n<-length(dims)
-if (any(cell>dims) || any(cell<1)) stop("No such cell")
-.C("aplDecodeC",as.integer(cell),as.integer(dims),as.integer(n),as.integer(1))[[4]]
-}
+#aplDecode <-
+#function(cell,dims) {
+#n<-length(dims)
+#if (any(cell>dims) || any(cell<1)) stop("No such cell")
+#.C("aplDecodeC",as.integer(cell),as.integer(dims),as.integer(n),as.integer(1))[[4]]
+#}
 
+aplDecode <- function( cell, dims ){
+    if( length( cell ) != length( dims ) ){
+       stop( "Dimension error" )
+    }
+    if( any(cell > dims ) || any ( cell < 1 ) ){
+       stop( "No such cell" )
+    }
+    .Call( "APLDECODE", as.integer( cell ), as.integer( dims ) )
+}
