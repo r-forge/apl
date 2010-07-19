d549f802060d8cf2b1dcef38c40fabb1a410d503
masanao (Masanao Yajima) <yajima@stat.ucla.edu>
2010-07-19 18:52:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/apl@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aplEXV.R
+++ pkg/R/aplEXV.R
@@ -1,10 +1,10 @@
-aplEXV <-
-function(x,y) {
-z<-rep(0,length(y))
-m<-which(y)
-if (length(m) != length(x))
-    stop("Incorrect vector length")
-z[which(y)]<-x
-return(z)
+aplEXV <- function( x, y ){
+    z <- rep( 0, length( y ) )
+    m <- which( y )
+    if( length( m ) != length( x ) ){
+        stop("Incorrect vector length")
+    }
+    z[which( y )] <- x
+    return( z )
 }
 
--- pkg/R/aplExpand.R
+++ pkg/R/aplExpand.R
@@ -1,10 +1,10 @@
-aplExpand <-
-function(x,y,axis=1) {
-    if (is.vector(x)) return(aplEXV(x,y))
-    d<-dim(x); m<-which(y); e<-d; e[axis]<-m
-    if (m != d[axis])
+aplExpand <- function( x, y, axis = 1 ){
+    if( is.vector( x ) ){ return( aplEXV( x, y ) ) }
+    d <- dim( x ); m <- which( y ); e <- d; e[axis] <- m;
+    if( m != d[axis] ){
         stop("Incorrect dimension length")
-    z<-array(0,e)
-    apply(z,(1:n)[-axis],function(i) z[i]<-x[i])
+    }
+    z <- array( 0, e )
+    apply( z,( 1:n )[-axis],function(i) z[i]<-x[i])
 }
 
