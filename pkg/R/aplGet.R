000bbb5ec5d286afbd25e6a7dad024fcdac24030
masanao (Masanao Yajima) <yajima@stat.ucla.edu>
2010-07-25 17:24:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/apl@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aplGet.R
+++ pkg/R/aplGet.R
@@ -3,5 +3,5 @@ aplGet <- function( a, cell )
     dims <- dim( a ); n <- length( dims ); b <- 0;
     if( any( cell > dims ) || any( cell < 1 ) ){ stop("No such cell"); }
     return( a[aplDecode( cell, dims )]);
-}
+} 
 
